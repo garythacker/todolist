bad796e1bdf94651cc76d7fad40f451c0d142a6e
garythacker <garythacker227@gmail.com>
2014-02-26 18:48:56 +0200

    add registration

--- tasks/views.py
+++ tasks/views.py
@@ -1,29 +1,59 @@
-from django.http import HttpResponse, Http404
-from django.template import Context
-from django.template.loader import get_template
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+from django.contrib.auth import logout
+from django.template import RequestContext
+from tasks.forms import RegistrationForm, TaskListSaveForm
+from tasks.models import *
 
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-        'head_title': u'Django Bookmarks',
-        'page_title': u'Welcome to Django Bookmarks',
-        'page_body': u'Where you can store and share bookmarks!'
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'main_page.html', RequestContext(request)
+    )
 
-def user_page(request, username):
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        raise Http404(u'Requested user not found.')
-    tasklist = user.tasklist_set.all()
-    template = get_template('user_page.html')
-    variables = Context({
-        'username': username,
-        'tasklist': tasklist
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+        'form': form
     })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'registration/register.html', variables
+    )
+
+def todolist_save_page(request):
+    if request.method == 'POST':
+        form = TaskListSaveForm(request.POST)
+        if form.is_valid():
+            task, dummy = Task.objects.get_or_create(
+                title=form.cleaned_data['title']
+            )
+            todolist, created = TaskList.objects.get_or_create(
+                user=request.user,
+                task=task
+            )
+            todolist.title = form.cleaned_data['title']
+            todolist.save()
+            return HttpResponseRedirect(
+                '/user/%s/' % request.user.username
+            )
+    else:
+        form = TaskListSaveForm()
+    variables = RequestContext(request, {
+        'form': form
+    })
+    return render_to_response('todolist_save.html', variables)
--- todolist/urls.py
+++ todolist/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import patterns, include, url
-from tasks.views import main_page, user_page
+from django.views.generic import TemplateView
+from tasks.views import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', main_page),
-    url(r'^user/(\w+)/$', user_page),
+   url(r'^$', main_page),
+
+   # Session management
+   url(r'^login/$', 'django.contrib.auth.views.login'),
+   url(r'^logout/$', logout_page),
+   url(r'^register/$', register_page),
+   url(r'^register/success/$', TemplateView.as_view(template_name="registration/register_success.html")),
+
+   # Account management
+   (r'^save/$', todolist_save_page),
 )
